1fa480d22307a702b801d938aa25759ee548ee2d
thomas070605 <thomas.bamberger.070605@gmail.com>
2021-08-03 14:15:16 +0200

    Initial commit

---
+++ src/main/java/com/wds/example/htlw/HtlwApplication.java
@@ -0,0 +1,13 @@
+package com.wds.example.htlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wds/example/htlw/HtlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wds.example.htlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
