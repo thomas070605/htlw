0237e18e081966450fb441ceb8d7f215314ebe2e
thomas070605 <thomas.bamberger.070605@gmail.com>
2021-08-04 17:31:32 +0200

    1

--- src/main/java/com/wds/example/htlw/domain/Person.java
+++ src/main/java/com/wds/example/htlw/domain/Person.java
@@ -1,62 +1,25 @@
 package com.wds.example.htlw.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
     @Id
      private int id;
 
     private String name;
 
-    private String lastname;
+    private String lastName;
 
-    private LocalDate birthdate;
+    private LocalDate birthDate;
 
-    public int getId() {
-        return id;
-    }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public LocalDate getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(LocalDate birthdate) {
-        this.birthdate = birthdate;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return id == person.id && Objects.equals(name, person.name) && Objects.equals(lastname, person.lastname) && Objects.equals(birthdate, person.birthdate);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, lastname, birthdate);
-    }
 }
