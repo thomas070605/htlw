07aee767e419168f994c740749b944603ddb807c
thomas070605 <thomas.bamberger.070605@gmail.com>
2021-08-05 14:57:28 +0200

    1

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -1,7 +1,9 @@
 create table person
 (
-    id integer not null primary key auto_increment,
+    id integer not null primary key ,
     name varchar,
     last_name varchar,
     birth_date date
 );
+
+insert into person values (1, 'Test', 'Tester', '2000-01-01');
